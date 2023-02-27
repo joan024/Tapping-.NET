c5396c4b39854da6315a26dac442d8dc2c57aa3a
joan024 <joan024@LAPTOP-7E3GBULT>
2023-02-27 20:28:02 +0100

    pantalla inici de l'aplicació d'escriptori per les empreses

--- Tapping/Form1.vb
+++ Tapping/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+
+    End Sub
 End Class
