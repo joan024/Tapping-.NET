2b6deca02f1bd25cd27fb1cdfe770ddb9422c124
joseprogramador1 <j.medina.1@insjoanbrudieu.cat>
2023-02-27 16:00:55 +0100

    Retornar abans

--- Tapping/Form1.vb
+++ Tapping/Form1.vb
@@ -1,9 +1,5 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("Jose Toy")
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
     End Sub
 End Class
