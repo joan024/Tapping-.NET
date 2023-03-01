9f8a6adcf416ef205530e429b106499a6518e8f7
joan024 <joan024@LAPTOP-7E3GBULT>
2023-03-01 20:20:15 +0100

    Disseny pantalla casi finalitzat

--- Tapping/Form1.Designer.vb
+++ Tapping/Form1.Designer.vb
@@ -22,17 +22,144 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GestionarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClientsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TapesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AfegirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LlistarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EliminarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AfegirToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditarToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LlistarToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EliminarToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.XatToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NotíciesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(177, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.MenuStrip1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GestionarToolStripMenuItem, Me.XatToolStripMenuItem, Me.NotíciesToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(78, 337)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'GestionarToolStripMenuItem
+        '
+        Me.GestionarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClientsToolStripMenuItem, Me.TapesToolStripMenuItem})
+        Me.GestionarToolStripMenuItem.Name = "GestionarToolStripMenuItem"
+        Me.GestionarToolStripMenuItem.Size = New System.Drawing.Size(65, 19)
+        Me.GestionarToolStripMenuItem.Text = "Gestionar "
+        '
+        'ClientsToolStripMenuItem
+        '
+        Me.ClientsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AfegirToolStripMenuItem, Me.EditarToolStripMenuItem, Me.LlistarToolStripMenuItem, Me.EliminarToolStripMenuItem})
+        Me.ClientsToolStripMenuItem.Name = "ClientsToolStripMenuItem"
+        Me.ClientsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClientsToolStripMenuItem.Text = "Clients"
+        '
+        'TapesToolStripMenuItem
+        '
+        Me.TapesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AfegirToolStripMenuItem1, Me.EditarToolStripMenuItem1, Me.LlistarToolStripMenuItem1, Me.EliminarToolStripMenuItem1})
+        Me.TapesToolStripMenuItem.Name = "TapesToolStripMenuItem"
+        Me.TapesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TapesToolStripMenuItem.Text = "Tapes"
+        '
+        'AfegirToolStripMenuItem
+        '
+        Me.AfegirToolStripMenuItem.Name = "AfegirToolStripMenuItem"
+        Me.AfegirToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AfegirToolStripMenuItem.Text = "Afegir"
+        '
+        'EditarToolStripMenuItem
+        '
+        Me.EditarToolStripMenuItem.Name = "EditarToolStripMenuItem"
+        Me.EditarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EditarToolStripMenuItem.Text = "Editar"
+        '
+        'LlistarToolStripMenuItem
+        '
+        Me.LlistarToolStripMenuItem.Name = "LlistarToolStripMenuItem"
+        Me.LlistarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.LlistarToolStripMenuItem.Text = "Llistar"
+        '
+        'EliminarToolStripMenuItem
+        '
+        Me.EliminarToolStripMenuItem.Name = "EliminarToolStripMenuItem"
+        Me.EliminarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EliminarToolStripMenuItem.Text = "Eliminar"
+        '
+        'AfegirToolStripMenuItem1
+        '
+        Me.AfegirToolStripMenuItem1.Name = "AfegirToolStripMenuItem1"
+        Me.AfegirToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.AfegirToolStripMenuItem1.Text = "Afegir"
+        '
+        'EditarToolStripMenuItem1
+        '
+        Me.EditarToolStripMenuItem1.Name = "EditarToolStripMenuItem1"
+        Me.EditarToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.EditarToolStripMenuItem1.Text = "Editar"
+        '
+        'LlistarToolStripMenuItem1
+        '
+        Me.LlistarToolStripMenuItem1.Name = "LlistarToolStripMenuItem1"
+        Me.LlistarToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.LlistarToolStripMenuItem1.Text = "Llistar"
+        '
+        'EliminarToolStripMenuItem1
+        '
+        Me.EliminarToolStripMenuItem1.Name = "EliminarToolStripMenuItem1"
+        Me.EliminarToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.EliminarToolStripMenuItem1.Text = "Eliminar"
+        '
+        'XatToolStripMenuItem
+        '
+        Me.XatToolStripMenuItem.Name = "XatToolStripMenuItem"
+        Me.XatToolStripMenuItem.Size = New System.Drawing.Size(65, 19)
+        Me.XatToolStripMenuItem.Text = "Xat"
+        '
+        'NotíciesToolStripMenuItem
+        '
+        Me.NotíciesToolStripMenuItem.Name = "NotíciesToolStripMenuItem"
+        Me.NotíciesToolStripMenuItem.Size = New System.Drawing.Size(65, 19)
+        Me.NotíciesToolStripMenuItem.Text = "Notícies"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(700, 337)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents GestionarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClientsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AfegirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LlistarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EliminarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TapesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AfegirToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents EditarToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents LlistarToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents EliminarToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents XatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NotíciesToolStripMenuItem As ToolStripMenuItem
 End Class
