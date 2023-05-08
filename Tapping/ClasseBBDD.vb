869d0092e7c280de97bb8708dbb9dd6a54acacbd
joseprogramador1 <j.medina.1@insjoanbrudieu.cat>
2023-05-08 19:19:58 +0200

    ClasseBBDD como master

--- Tapping/ClasseBBDD.vb
+++ Tapping/ClasseBBDD.vb
@@ -1,5 +1,6 @@
-﻿Imports System.IO
+﻿Imports System.Globalization
 Imports MySql.Data.MySqlClient
+Imports Mysqlx.XDevAPI.Relational
 
 Public Class ClasseBBDD
     Private connexio As MySqlConnection
@@ -37,7 +38,7 @@ Public Class ClasseBBDD
         Dim reader = comanda.ExecuteReader()
         Return reader
     End Function
-    Public Sub SelectAdmin(ByVal taula As String, ByVal dgv As DataGridView)
+    Public Sub mostrarAdmin(ByVal taula As String, ByVal dgv As DataGridView)
         sentencia = "SELECT * FROM " & taula
         Try
             table = New DataTable
@@ -46,39 +47,35 @@ Public Class ClasseBBDD
             adapter.Fill(table)
             dgv.DataSource = table
         Catch ex As Exception
-            MessageBox.Show("Error en 'SelectAdmin' ClasseBBDD: " & ex.Message)
+            MessageBox.Show("Error en 'mostrarAdmin' ClasseBBDD: " & ex.Message)
         Finally
             desconnectar()
         End Try
     End Sub
-    Public Sub UpdateAdmin(ByVal taula As String, ByVal dades() As String, ByVal id As Integer, ByVal dgv As DataGridView)
-        Select Case taula
-            Case "noticia"
-                sentencia = "UPDATE noticia SET titol = '" & dades(0) & "', descripcio = '" & dades(1) & "', foto = '" & dades(2) & "', data_publicacio = '" & dades(3) & "', data_inici = '" & dades(4) & "', data_fi = '" & dades(5) & "' WHERE id = " & id
-            Case "preguntafrequent"
-                sentencia = "UPDATE preguntafrequent SET pregunta = '" & dades(0) & "', resposta = '" & dades(1) & "' WHERE id = " & id
-        End Select
+    Public Sub modificarAdmin(ByVal taula As String, ByVal dgv As DataGridView, ByVal numColMod As Integer, ByVal text As String, ByVal numColId As Integer, ByVal id As Integer)
+        sentencia = "UPDATE " & taula & " SET " & dgv.Columns(numColMod).Name & "= '" & text & "' WHERE " & dgv.Columns(numColId).Name & " = " & id
         Try
             connectar()
             comanda = New MySqlCommand(sentencia, connexio)
             If comanda.ExecuteNonQuery() Then
-                SelectAdmin(taula, dgv)
+                mostrarAdmin(taula, dgv)
             Else
-                MsgBox("ERROR en fer la modificació", vbExclamation)
+                MsgBox("ERROR al fer la modificació", vbExclamation)
             End If
         Catch ex As Exception
-            MessageBox.Show("Error 'UpdateAdmin' ClasseBBDD: " & ex.Message)
+            MessageBox.Show("Error en 'modificarAdmin' ClasseBBDD: " & ex.Message)
         Finally
             desconnectar()
         End Try
     End Sub
-    Public Sub InsertAdmin(ByVal taula As String, ByVal dades() As String, ByVal dgv As DataGridView)
+
+    Public Sub afegirAdmin(ByVal taula As String, ByVal dades() As String, ByVal dgv As DataGridView)
         Select Case taula
             Case "noticia"
-                If dades(5) = "" Then
-                    sentencia = "INSERT INTO noticia (titol,descripcio,foto,data_publicacio,data_inici,data_fi) VALUES ('" & dades(0) & "','" & dades(1) & "','" & dades(2) & "','" & dades(3) & "','" & dades(4) & "', NULL)"
-                Else
+                If dades.Length = 6 Then
                     sentencia = "INSERT INTO noticia (titol,descripcio,foto,data_publicacio,data_inici,data_fi) VALUES ('" & dades(0) & "','" & dades(1) & "','" & dades(2) & "','" & dades(3) & "','" & dades(4) & "','" & dades(5) & "')"
+                Else
+                    sentencia = "INSERT INTO noticia (titol,descripcio,foto,data_publicacio,data_inici,data_fi) VALUES ('" & dades(0) & "','" & dades(1) & "','" & dades(2) & "','" & dades(3) & "','" & dades(4) & "')"
                 End If
             Case "preguntafrequent"
                 sentencia = "INSERT INTO preguntafrequent (pregunta,resposta) VALUES ('" & dades(0) & "','" & dades(1) & "')"
@@ -90,197 +87,14 @@ Public Class ClasseBBDD
             connectar()
             comanda = New MySqlCommand(sentencia, connexio)
             If comanda.ExecuteNonQuery() > 0 Then
-                SelectAdmin(taula, dgv)
+                mostrarAdmin(taula, dgv)
             Else
                 MsgBox("ERROR al fer la inserció", vbExclamation)
             End If
         Catch ex As Exception
-            MessageBox.Show("Error 'InsertAdmin' ClasseBBDD: " & ex.Message)
+            MessageBox.Show("Error 'afegirAdmin' ClasseBBDD: " & ex.Message)
         Finally
             desconnectar()
         End Try
     End Sub
-    Public Sub Delete(ByVal taula As String, ByVal dgv As DataGridView, ByVal id As String)
-        sentencia = "DELETE FROM " & taula & " WHERE id = " & id
-        Try
-            connectar()
-            comanda = New MySqlCommand(sentencia, connexio)
-            If comanda.ExecuteNonQuery() Then
-                SelectAdmin(taula, dgv)
-            Else
-                MsgBox("ERROR en fer l'eliminació", vbExclamation)
-            End If
-        Catch ex As Exception
-            MessageBox.Show("Error 'Delete' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-    Public Sub SelectEmpresa(ByVal taula As String, ByVal id As String, ByVal dgv As DataGridView)
-        Select Case taula
-            Case "noticia"
-                sentencia = "SELECT titol,descripcio,foto FROM " & taula & " WHERE data_inici <= '" & Format(Date.Today, "yyyy-MM-dd") & "' AND (data_fi >= '" & Format(Date.Today, "yyyy-MM-dd") & "' OR data_fi IS NULL)"
-            Case "preguntafrequent"
-                sentencia = "SELECT pregunta,resposta FROM " & taula & ""
-            Case "valoracio"
-                sentencia = "SELECT puntuacio,comentari FROM " & taula & " WHERE id_local = '" & id & "'"
-        End Select
-        Try
-            table = New DataTable
-            connectar()
-            adapter = New MySqlDataAdapter(sentencia, connexio)
-            adapter.Fill(table)
-            dgv.DataSource = table
-        Catch ex As Exception
-            MessageBox.Show("Error en 'SelectEmpresa' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-    Public Sub XatsEmpresa(ByVal dgv As DataGridView)
-        sentencia = "SELECT nom FROM usuari INNER JOIN xat ON usuari.id = xat.id_empresa"
-        Try
-            table = New DataTable
-            connectar()
-            adapter = New MySqlDataAdapter(sentencia, connexio)
-            adapter.Fill(table)
-            dgv.DataSource = table
-        Catch ex As Exception
-            MessageBox.Show("Error 'XatsEmpresa' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-    Public Sub MissatgesXatAdmin(ByVal empresa As String, ByVal dgv As DataGridView)
-        sentencia = "SELECT lineaxat.usuari,lineaxat.missatge,lineaxat.id_xat FROM lineaxat INNER JOIN xat ON lineaxat.id_xat = xat.id INNER JOIN usuari ON xat.id_empresa = usuari.id WHERE usuari.nom = '" & empresa & "'"
-        Try
-            table = New DataTable
-            connectar()
-            adapter = New MySqlDataAdapter(sentencia, connexio)
-            adapter.Fill(table)
-            dgv.DataSource = table
-        Catch ex As Exception
-            MessageBox.Show("Error 'MissatgesXatAdmin' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-    Public Sub InsertMissatgeAdmin(ByVal idXat As String, ByVal missatge As String, ByVal empresa As String, ByVal dgv As DataGridView)
-        sentencia = "INSERT INTO lineaxat (id_xat,usuari,missatge,temps) VALUES (" & idXat & ",1,'" & missatge & "','" & Format(Date.Today, "yyyy-MM-dd") & "')"
-        Try
-            connectar()
-            comanda = New MySqlCommand(sentencia, connexio)
-            If comanda.ExecuteNonQuery() > 0 Then
-                MissatgesXatAdmin(empresa, dgv)
-            Else
-                MsgBox("ERROR en fer la inserció del missatge", vbExclamation)
-            End If
-        Catch ex As Exception
-            MessageBox.Show("Error 'InsertMissatgeAdmin' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-    Public Sub MissatgesXatEmpresa(ByVal id As String, ByVal dgv As DataGridView)
-        sentencia = "SELECT lineaxat.usuari,lineaxat.missatge,lineaxat.id_xat FROM lineaxat INNER JOIN xat ON lineaxat.id_xat = xat.id WHERE xat.id_empresa = '" & id & "'"
-        Try
-            table = New DataTable
-            connectar()
-            adapter = New MySqlDataAdapter(sentencia, connexio)
-            adapter.Fill(table)
-            dgv.DataSource = table
-        Catch ex As Exception
-            MessageBox.Show("Error 'MissatgesXatEmpresa' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-    Public Sub InsertMissatgeEmpresa(ByVal idXat As String, ByVal missatge As String, ByVal id As String, ByVal dgv As DataGridView)
-        sentencia = "INSERT INTO lineaxat (id_xat,usuari,missatge,temps) VALUES (" & idXat & ",2,'" & missatge & "','" & Format(Date.Today, "yyyy-MM-dd") & "')"
-        Try
-            connectar()
-            comanda = New MySqlCommand(sentencia, connexio)
-            If comanda.ExecuteNonQuery() > 0 Then
-                MissatgesXatEmpresa(id, dgv)
-            Else
-                MsgBox("ERROR en fer la inserció del missatge", vbExclamation)
-            End If
-        Catch ex As Exception
-            MessageBox.Show("Error 'InsertMissatgeEmpresa' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-    Public Sub ArxiuLocal()
-        sentencia = "SELECT id_local,id,extensio FROM foto WHERE id = (SELECT MAX(id) FROM foto)"
-        Try
-            connectar()
-            comanda = New MySqlCommand(sentencia, connexio)
-            Dim reader As MySqlDataReader = comanda.ExecuteReader()
-            While reader.Read()
-                Dim path As String = "C:\TappingFotos\local\" & reader("id_local") & ".txt"
-                If File.Exists(path) Then
-                    Using writer As New StreamWriter(path, True)
-                        writer.WriteLine(reader("id") & "" & reader("extensio"))
-                    End Using
-                Else
-                    Using writer As New StreamWriter(path)
-                        writer.WriteLine(Now & "#" & reader("id_local"))
-                        writer.WriteLine(reader("id") & "" & reader("extensio"))
-                    End Using
-                End If
-            End While
-        Catch ex As Exception
-            MessageBox.Show("Error 'ArxiuLocal' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-    Public Sub PujarFoto(ByVal dades() As String)
-        sentencia = "INSERT INTO foto (id_local,extensio) VALUES ('" & dades(0) & "','" & dades(1) & "')"
-        Try
-            connectar()
-            comanda = New MySqlCommand(sentencia, connexio)
-            comanda.ExecuteNonQuery()
-        Catch ex As Exception
-            MessageBox.Show("Error 'PujarFoto' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-    End Sub
-
-    Public Function SelectFoto() As String
-        sentencia = "SELECT MAX(id) FROM foto"
-        Try
-            connectar()
-            comanda = New MySqlCommand(sentencia, connexio)
-            Dim reader As MySqlDataReader = comanda.ExecuteReader()
-            If reader.Read() Then
-                Dim maxId As Integer = reader.GetInt32(0)
-                Return maxId.ToString()
-            End If
-        Catch ex As Exception
-            MessageBox.Show("Error 'SelectFoto' ClasseBBDD: " & ex.Message)
-        Finally
-            desconnectar()
-        End Try
-        Return 0
-    End Function
-
-    Public Sub DeleteFoto(ByVal id As String)
-        sentencia = "DELETE FROM foto WHERE id = " & id
-        Try
-            connectar()
-            comanda = New MySqlCommand(sentencia, connexio)
-            Dim reader As MySqlDataReader = comanda.ExecuteReader()
-            If reader.Read() Then
-                Dim maxId As Integer = reader.GetInt32(0)
-            End If
-        Catch ex As Exception
-            MessageBox.Show("Error 'DeleteFoto' ClasseBBDD: " & ex.Message)
-        Finally
-        desconnectar()
-        End Try
-    End Sub
-
 End Class
