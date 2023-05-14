1069f7fb001734e2ac795dd5cdad6b20f2b9946d
joseprogramador1 <j.medina.1@insjoanbrudieu.cat>
2023-05-15 01:30:51 +0200

    form categories no s'utilitza

--- Tapping/FormCategoria.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FormCategoria
-    Private Sub FormCategoria_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        DataGridViewCategoria.Rows.Add("1", "Bocadillos")
-        DataGridViewCategoria.Rows.Add("2", "Ensaladas")
-    End Sub
-End Class
